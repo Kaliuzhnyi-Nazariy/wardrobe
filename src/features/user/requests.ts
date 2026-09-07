30ceaf2752a25e947379c7f5c776947de1a9aed4
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-09-07 11:40:41 +0200

    feat: add upd and delete user data

--- src/features/user/requests.ts
+++ src/features/user/requests.ts
@@ -3,3 +3,28 @@ import { api } from "../api";
 export const getData = async () => {
   return (await api.get("/user")).data;
 };
+
+export const updateUserData = async ({
+  name,
+  email,
+}: {
+  name: string;
+  email: string;
+}) => {
+  return (await api.put("/user", { name, email })).data;
+};
+
+export const updateUserPassword = async ({
+  password,
+  confirmPassword,
+}: {
+  password: string;
+  confirmPassword: string;
+}) => {
+  return (await api.patch("/user/password", { password, confirmPassword }))
+    .data;
+};
+
+export const deleteUserAccount = async () => {
+  return await api.delete("/user");
+};
